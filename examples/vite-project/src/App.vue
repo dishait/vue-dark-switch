10239052c23763581f9dafb4ccee70fd80e968bc
markthree <1801982702@qq.com>
2022-04-25 17:21:18 +0800

    chore: add example

---
+++ examples/vite-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Switch } from 'vue-dark-switch'
+</script>
+
+<template>
+	<Switch />
+</template>
+
+<style>
+#app {
+	height: 20vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
