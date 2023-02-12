36931d089da2249057cf2bc475d3242c277e39c4
markthree <1801982702@qq.com>
2023-02-12 13:57:00 +0800

    build: add turbo

--- src/exports.ts
+++ src/exports.ts
@@ -1,15 +1,7 @@
-import 'uno.css'
+import "uno.css";
 
-export { default as Switch } from './components/switch.vue'
-export { default as SwitchIcon } from './components/switch-icon.vue'
+export { default as Switch } from "./components/switch.vue";
+export { default as SwitchIcon } from "./components/switch-icon.vue";
 
-export {
-	isDark,
-	naiveTheme,
-	toggleDark
-} from './composables/theme'
-export {
-	mountStyle,
-	mountScript,
-	mountPresets
-} from './composables/mount'
+export { isDark, naiveTheme, toggleDark } from "./composables/theme";
+export { mountPresets, mountScript, mountStyle } from "./composables/mount";
