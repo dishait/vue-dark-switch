fd43694fc7a633e5f2f43d9104a4f77d9ab1f87d
markthree <1801982702@qq.com>
2023-03-28 21:18:49 +0800

    chore: remove copy-polyfill plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,30 @@
-import Unocss from 'unocss/vite'
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import { builtinModules } from 'node:module'
+import Unocss from "unocss/vite";
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import { builtinModules } from "node:module";
 
 export default defineConfig({
-	build: {
-		emptyOutDir: false,
-		lib: {
-			name: 'vue-dark-switch',
-			formats: ['cjs', 'es'],
-			fileName(f, e) {
-				const ext = f === 'es' ? 'mjs' : f
-				return `${e}.${ext}`
-			},
-			entry: ['./src/exports.ts', './src/vite.ts']
-		},
-		rollupOptions: {
-			external: [
-				'vue',
-				...builtinModules,
-				...builtinModules.map(m => `node:${m}`)
-			]
-		}
-	},
-	plugins: [
-		Vue(),
-		Unocss(),
-		{
-			name: 'copy-polyfill',
-			apply: 'build',
-			async buildEnd() {
-				const { default: cpy } = await import('cpy')
-				cpy('src/polyfill/**/*', 'dist/polyfill')
-			}
-		}
-	]
-})
+  build: {
+    emptyOutDir: false,
+    lib: {
+      name: "vue-dark-switch",
+      formats: ["cjs", "es"],
+      fileName(f, e) {
+        const ext = f === "es" ? "mjs" : f;
+        return `${e}.${ext}`;
+      },
+      entry: ["./src/exports.ts", "./src/vite.ts"],
+    },
+    rollupOptions: {
+      external: [
+        "vue",
+        ...builtinModules,
+        ...builtinModules.map((m) => `node:${m}`),
+      ],
+    },
+  },
+  plugins: [
+    Vue(),
+    Unocss(),
+  ],
+});
